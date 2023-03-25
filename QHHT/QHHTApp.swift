70609d17f0cba3deb7eee5c3b1743659bdb26e99
alexnegreteqhht <me@alexnegrete.com>
2023-03-24 20:34:53 -0500

    Initial Commit

---
+++ QHHT/QHHTApp.swift
@@ -0,0 +1,17 @@
+//
+//  QHHTApp.swift
+//  QHHT
+//
+//  Created by Alex Negrete on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct QHHTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
